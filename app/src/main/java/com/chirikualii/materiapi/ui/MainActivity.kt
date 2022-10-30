d5fcc871f8637bb4bb8c5707aaed5cd6a838a897
Habi <habijiyan111@gmail.com>
2022-10-30 14:55:41 +0700

    Menghapusa function loadData from API dan Menambah Progres Bar

--- app/src/main/java/com/chirikualii/materiapi/ui/MainActivity.kt
+++ app/src/main/java/com/chirikualii/materiapi/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.chirikualii.materiapi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.chirikualii.materiapi.R
 import com.chirikualii.materiapi.data.dummy.DataDummy
 import com.chirikualii.materiapi.data.model.Movie
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MovieListAdapter
+
+    private val mViewModel: MainViewModel by viewModels(
+        factoryProducer = { MainViewModelFactory() }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -27,42 +34,12 @@ class MainActivity : AppCompatActivity() {
         adapter = MovieListAdapter()
         binding.rvMovie.adapter = adapter
 
-    }
+        mViewModel.doGetPopularMovie()
+        binding.progressBar.visibility = View.VISIBLE
 
-    private fun loadDataFromApi() {
-        val service = ApiClient.service
-
-        GlobalScope.launch(Dispatchers.IO) {
-            try {
-                val response = service.getPopularMovie()
-
-                if (response.isSuccessful) {
-                    withContext(Dispatchers.Main) {
-                        val listMovie = response.body()?.results?.map {
-                            Movie(
-                                title = it.title,
-                                genre = it.releaseDate,
-                                imagePoster = it.posterPath
-                            )
-                        }
-                        withContext(Dispatchers.Main) {
-                            if (listMovie != null) {
-                                adapter.addItem(listMovie)
-                            }
-                        }
-
-
-                    }
-
-                } else {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, "gagal", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            } catch (e: Exception) {
-
-            }
-
+        mViewModel.listMovie.observe(this) {
+            binding.progressBar.visibility = View.INVISIBLE
+            adapter.addItem(it)
         }
     }
 }
